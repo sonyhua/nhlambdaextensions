f3b55524928a310546cba41d6db615306e75d923
FlukeFan <FlukeFan@33c19e34-bb07-11dd-a828-c573283ffd12>
2008-11-25 17:03:11 +0000

    Added initial tests and simplest implementation.

---
+++ Source/Test/Person.cs
@@ -0,0 +1,14 @@
+
+using System;
+
+namespace NHibernate.LambdaExtensions.Test
+{
+
+    public class Person
+    {
+        public string Name { get; protected set; }
+        public int Age { get; protected set; }
+    }
+
+}
+
---
+++ Source/Test/TestCriteria.cs
@@ -0,0 +1,80 @@
+
+using System;
+
+using NHibernate;
+using NHibernate.Criterion;
+
+using NUnit.Framework;
+
+namespace NHibernate.LambdaExtensions.Test
+{
+
+    [TestFixture]
+    public class TestCriteria : TestBase
+    {
+
+        private ICriteria CreateCriteria<T>()
+        {
+            return new NHibernate.Impl.CriteriaImpl(typeof(T), null);
+        }
+
+        [Test]
+        public void TestEq()
+        {
+            ICriteria expected =
+                CreateCriteria<Person>()
+                    .Add(Expression.Eq("Name", "test name"));
+
+            ICriteria actual =
+                CreateCriteria<Person>()
+                    .Add<Person>(p => p.Name == "test name");
+
+            AssertCriteriaAreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestSimpleEqWithMemberExpression()
+        {
+            ICriteria expected =
+                CreateCriteria<Person>()
+                    .Add(Expression.Eq("Name", "test name"));
+
+            string name = "test name";
+            ICriteria actual =
+                CreateCriteria<Person>()
+                    .Add<Person>(p => p.Name == name);
+
+            AssertCriteriaAreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestSimpleGt()
+        {
+            ICriteria expected =
+                CreateCriteria<Person>()
+                    .Add(Expression.Gt("Age", 10));
+
+            ICriteria actual =
+                CreateCriteria<Person>()
+                    .Add<Person>(p => p.Age > 10);
+
+            AssertCriteriaAreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestOrderAsc()
+        {
+            ICriteria expected =
+                CreateCriteria<Person>()
+                    .AddOrder(Order.Desc("Name"));
+
+            ICriteria actual =
+                CreateCriteria<Person>()
+                    .AddOrder<Person>(p => p.Name, Order.Desc);
+
+            AssertCriteriaAreEqual(expected, actual);
+        }
+
+    }
+
+}
