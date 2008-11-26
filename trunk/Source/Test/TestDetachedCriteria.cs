f4a30fc2c80cef66f9bbe37a3bb1f0c3d2a10adf
FlukeFan <FlukeFan@33c19e34-bb07-11dd-a828-c573283ffd12>
2008-11-26 10:08:07 +0000

    Added extensions for DetachedCriteria

---
+++ trunk/Source/Test/TestDetachedCriteria.cs
@@ -0,0 +1,65 @@
+
+using System;
+
+using NHibernate.Criterion;
+
+using NUnit.Framework;
+
+namespace NHibernate.LambdaExtensions.Test
+{
+
+    [TestFixture]
+    public class TestDetachedCriteria : TestBase
+    {
+
+        private void AssertCriteriaAreEqual(DetachedCriteria expected, DetachedCriteria actual)
+        {
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+        [Test]
+        public void TestAdd()
+        {
+            DetachedCriteria expected =
+                DetachedCriteria.For<Person>()
+                    .Add(Expression.Eq("Name", "test name"));
+
+            DetachedCriteria actual =
+                DetachedCriteria.For<Person>()
+                    .Add<Person>(p => p.Name == "test name");
+
+            AssertCriteriaAreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestOrder()
+        {
+            DetachedCriteria expected =
+                DetachedCriteria.For<Person>()
+                    .AddOrder(Order.Desc("Name"));
+
+            DetachedCriteria actual =
+                DetachedCriteria.For<Person>()
+                    .AddOrder<Person>(p => p.Name, Order.Desc);
+
+            AssertCriteriaAreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCombinedAddAndOrder()
+        {
+            DetachedCriteria expected =
+                DetachedCriteria.For<Person>()
+                    .Add(Expression.Eq("Name", "test name"))
+                    .AddOrder(Order.Desc("Name"));
+
+            DetachedCriteria actual =
+                DetachedCriteria.For<Person>()
+                    .Add<Person>(p => p.Name == "test name");
+
+            AssertCriteriaAreEqual(expected, actual);
+        }
+
+    }
+
+}
