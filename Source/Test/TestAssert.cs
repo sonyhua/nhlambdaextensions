b035fee2578fabc0fb0ffca02a09782b29ce46b3
FlukeFan <FlukeFan@33c19e34-bb07-11dd-a828-c573283ffd12>
2009-01-31 17:33:59 +0000

    Removed serialisation, and migrated to reflection to check equality of criteria.

---
+++ Source/Test/TestAssert.cs
@@ -0,0 +1,97 @@
+﻿
+using System;
+
+using NUnit.Framework;
+
+using NHibernate.Criterion;
+
+namespace NHibernate.LambdaExtensions.Test
+{
+
+    [TestFixture]
+    public class TestAssert : TestBase
+    {
+
+        private void AssertCriteriaAreNotEqual(ICriteria expected, ICriteria actual)
+        {
+            try
+            {
+                AssertCriteriaAreEqual(expected, actual);
+            }
+            catch
+            {
+                return;
+            }
+            Assert.Fail("No exception thrown");
+        }
+
+        private void AssertCriteriaAreNotEqual(DetachedCriteria expected, DetachedCriteria actual)
+        {
+            try
+            {
+                AssertCriteriaAreEqual(expected, actual);
+            }
+            catch
+            {
+                return;
+            }
+            Assert.Fail("No exception thrown");
+        }
+
+        [Test]
+        public void TestCheckTypes()
+        {
+            DetachedCriteria expected =
+                DetachedCriteria.For<Person>();
+
+            DetachedCriteria actual =
+                DetachedCriteria.For<Child>();
+
+            AssertCriteriaAreNotEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCheckOperators()
+        {
+            DetachedCriteria expected =
+                DetachedCriteria.For<Person>()
+                    .Add(Expression.Eq("Property", "Value"));
+
+            DetachedCriteria actual =
+                DetachedCriteria.For<Person>()
+                    .Add(Expression.Gt("Property", "Value"));
+
+            AssertCriteriaAreNotEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCheckPropertyNames()
+        {
+            DetachedCriteria expected =
+                DetachedCriteria.For<Person>()
+                    .Add(Expression.Eq("Property1", "Value"));
+
+            DetachedCriteria actual =
+                DetachedCriteria.For<Person>()
+                    .Add(Expression.Eq("Property2", "Value"));
+
+            AssertCriteriaAreNotEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCheckValues()
+        {
+            DetachedCriteria expected =
+                DetachedCriteria.For<Person>()
+                    .Add(Expression.Eq("Property", "Value1"));
+
+            DetachedCriteria actual =
+                DetachedCriteria.For<Person>()
+                    .Add(Expression.Eq("Property", "Value2"));
+
+            AssertCriteriaAreNotEqual(expected, actual);
+        }
+
+	}
+
+}
