47121676eb13ec6ad2aec12da728375a76a59e00
FlukeFan <FlukeFan@33c19e34-bb07-11dd-a828-c573283ffd12>
2009-01-31 18:29:55 +0000

    Removed 'old' method of testing, replacing with reflection to check internals of objects are equal.

--- trunk/Source/Test/TestBase.cs
+++ trunk/Source/Test/TestBase.cs
@@ -50,76 +50,6 @@ namespace NHibernate.LambdaExtensions.Test
             return session;
         }
 
-        private void AssertFetchModesAreEqual(IDictionary expected, IDictionary actual)
-        {
-            Assert.AreEqual(expected.Keys.Count, actual.Keys.Count, "FetchModes count mismatch");
-            foreach (string expectedAlias in expected.Keys)
-            {
-                Assert.IsTrue(actual.Contains(expectedAlias), "Expected alias " + expectedAlias);
-                FetchMode expectedFetchMode = (FetchMode)expected[expectedAlias];
-                FetchMode actualFetchMode = (FetchMode)actual[expectedAlias];
-                Assert.AreEqual(expectedFetchMode, actualFetchMode);
-            }
-        }
-
-        private void AssertLockModesAreEqual(IDictionary expected, IDictionary actual)
-        {
-            Assert.AreEqual(expected.Keys.Count, actual.Keys.Count, "LockModes count mismatch");
-            foreach (string expectedAlias in expected.Keys)
-            {
-                Assert.IsTrue(actual.Contains(expectedAlias), "Expected alias " + expectedAlias);
-                LockMode expectedLockMode = (LockMode)expected[expectedAlias];
-                LockMode actualLockMode = (LockMode)actual[expectedAlias];
-                Assert.AreEqual(expectedLockMode, actualLockMode);
-            }
-        }
-
-        private void AssertProjectionsAreEqual(IProjection expected, IProjection actual)
-        {
-            if (expected == null)
-            {
-                Assert.IsNull(actual, "expected no projection");
-                return;
-            }
-
-            Assert.IsNotNull(actual, "expected projection, got null");
-
-            Assert.AreEqual(expected.ToString(), actual.ToString());
-            if (expected is ProjectionList)
-            {
-                ProjectionList expectedList = (ProjectionList)expected;
-                ProjectionList actualList = (ProjectionList)actual;
-                Assert.AreEqual(expectedList.Length, actualList.Length, "Projection count mismatch");
-                for (int i=0; i<expectedList.Length; i++)
-                {
-                    AssertProjectionsAreEqual(expectedList[i], actualList[i]);
-                }
-            }
-        }
-
-        private string FindPropertyName(PropertySubqueryExpression subqueryExpression)
-        {
-            FieldInfo fieldInfo = typeof(PropertySubqueryExpression).GetField("propertyName", BindingFlags.NonPublic | BindingFlags.Instance);
-            return (string) fieldInfo.GetValue(subqueryExpression);
-        }
-
-        private void AssertRestrictonsAreEqual(IList expected, IList actual)
-        {
-            Assert.AreEqual(expected.Count, actual.Count, "Restrictons count mismatch");
-            for (int i=0; i<expected.Count; i++)
-            {
-                CriteriaImpl.CriterionEntry expectedCriterionEntry = (CriteriaImpl.CriterionEntry)expected[i];
-                CriteriaImpl.CriterionEntry actualCriterionEntry = (CriteriaImpl.CriterionEntry)actual[i];
-
-                if (expectedCriterionEntry.Criterion is PropertySubqueryExpression)
-                {
-                    string expectedProperty = FindPropertyName((PropertySubqueryExpression) expectedCriterionEntry.Criterion);
-                    string actualProperty = FindPropertyName((PropertySubqueryExpression) actualCriterionEntry.Criterion);
-                    Assert.AreEqual(expectedProperty, actualProperty);
-                }
-            }
-        }
-
         private void AssertDictionariesAreEqual(IDictionary expected, IDictionary actual)
         {
             Assert.AreEqual(expected.Keys.Count, actual.Keys.Count);
@@ -216,48 +146,11 @@ namespace NHibernate.LambdaExtensions.Test
 
         protected void AssertCriteriaAreEqual(ICriteria expected, ICriteria actual)
         {
-            expected = expected.GetCriteriaByAlias(expected.RootAlias);
-            actual = actual.GetCriteriaByAlias(actual.RootAlias);
-            Assert.AreEqual(expected.ToString(), actual.ToString());
-            Assert.AreEqual(expected.Alias, actual.Alias);
-            AssertFetchModesAreEqual(expected.FetchModes, actual.FetchModes);
-            AssertLockModesAreEqual(expected.LockModes, actual.LockModes);
-            AssertProjectionsAreEqual(expected.Projection, actual.Projection);
-            AssertRestrictonsAreEqual(expected.Restrictions, actual.Restrictions);
-            Assert.AreEqual(expected.SubcriteriaList.Count, actual.SubcriteriaList.Count, "Subcriteria count mismatch");
-
-            for (int i=0; i<expected.SubcriteriaList.Count; i++)
-            {
-                CriteriaImpl.Subcriteria expectedSubcriteria = (CriteriaImpl.Subcriteria)expected.SubcriteriaList[i];
-                CriteriaImpl.Subcriteria actualSubcriteria = (CriteriaImpl.Subcriteria)actual.SubcriteriaList[i];
-                Assert.AreEqual(expectedSubcriteria.Alias, actualSubcriteria.Alias);
-                Assert.AreEqual(expectedSubcriteria.Path, actualSubcriteria.Path);
-                Assert.AreEqual(expectedSubcriteria.JoinType, actualSubcriteria.JoinType);
-                Assert.AreEqual(expectedSubcriteria.ToString(), actualSubcriteria.ToString());
-            }
             AssertObjectsAreEqual(expected, actual);
         }
 
         protected void AssertCriteriaAreEqual(DetachedCriteria expected, DetachedCriteria actual)
         {
-            expected = expected.GetCriteriaByAlias(expected.RootAlias);
-            actual = actual.GetCriteriaByAlias(actual.RootAlias);
-            Assert.AreEqual(expected.ToString(), actual.ToString());
-            Assert.AreEqual(expected.Alias, actual.Alias);
-            AssertFetchModesAreEqual(expected.FetchModes, actual.FetchModes);
-            AssertProjectionsAreEqual(expected.Projection, actual.Projection);
-            AssertRestrictonsAreEqual(expected.Restrictions, actual.Restrictions);
-            Assert.AreEqual(expected.SubcriteriaList.Count, actual.SubcriteriaList.Count, "Subcriteria count mismatch");
-
-            for (int i=0; i<expected.SubcriteriaList.Count; i++)
-            {
-                CriteriaImpl.Subcriteria expectedSubcriteria = (CriteriaImpl.Subcriteria)expected.SubcriteriaList[i];
-                CriteriaImpl.Subcriteria actualSubcriteria = (CriteriaImpl.Subcriteria)actual.SubcriteriaList[i];
-                Assert.AreEqual(expectedSubcriteria.Alias, actualSubcriteria.Alias);
-                Assert.AreEqual(expectedSubcriteria.Path, actualSubcriteria.Path);
-                Assert.AreEqual(expectedSubcriteria.JoinType, actualSubcriteria.JoinType);
-                Assert.AreEqual(expectedSubcriteria.ToString(), actualSubcriteria.ToString());
-            }
             AssertObjectsAreEqual(expected, actual);
         }
 
