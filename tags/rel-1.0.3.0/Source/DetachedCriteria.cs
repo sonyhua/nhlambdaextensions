38395f341220a959db82123e113caf27ffcdd5c9
FlukeFan <FlukeFan@33c19e34-bb07-11dd-a828-c573283ffd12>
2009-02-28 10:17:21 +0000

    Tagged after release.

---
+++ tags/rel-1.0.3.0/Source/DetachedCriteria.cs
@@ -0,0 +1,42 @@
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+using NHibernate.Criterion;
+
+namespace NHibernate.LambdaExtensions
+{
+
+    /// <summary>
+    /// Provides extra factory method for DetachedCriteria to allow creation with an alias
+    /// </summary>
+    /// <remarks>
+    /// cannot add extension methods to simulate static method on DetachedCriteria
+    /// </remarks>
+    public class DetachedCriteria<T>
+    {
+
+        /// <summary>
+        /// Protected constructor - class not for instantiation
+        /// </summary>
+        protected DetachedCriteria() { }
+
+        /// <summary>
+        /// Create a DetachedCriteria with a strongly typed alias
+        /// </summary>
+        /// <param name="alias">Lambda expression returning alias reference</param>
+        /// <returns>Newly created DetachedCriteria</returns>
+        public static DetachedCriteria Create(Expression<Func<object>> alias)
+        {
+            string aliasContainer = ExpressionProcessor.FindMemberExpression(alias.Body);
+            return DetachedCriteria.For<T>(aliasContainer);
+        }
+
+    }
+
+}
+
